30e2551a17e58b941bdacab724fa1e65ed07b6e2
Elliot Robertson <elliotrobertson53@gmail.com>
2022-02-19 21:20:20 +0000

    finished review app

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import FeedbackForm from './components/FeedbackForm.svelte';
+  import FeedbackList from './components/FeedbackList.svelte';
+  import FeedbackStats from './components/FeedbackStats.svelte';
+  import Header from './components/Header.svelte';
+</script>
+
+<main class="container">
+  <Header />
+  <FeedbackForm />
+  <FeedbackStats />
+  <FeedbackList />
+</main>
---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<h1>Reviews 💻</h1>
+
+<style>
+  h1 {
+    text-align: center;
+    margin: 33px;
+    text-transform: uppercase;
+  }
+</style>
